f2cc39a73c7690a0827a9d0fe95120a84a2ccb70
Andreas Sousa <devandreas96@gmail.com>
2026-04-03 00:55:11 +0000

    fix: corrigir bug no arquivo prisma

---
+++ src/app/api/customers/route.ts
@@ -0,0 +1,25 @@
+import { NextRequest, NextResponse } from 'next/server';
+import { CustomerController } from '@/controllers/CustomerController';
+import { request } from 'http';
+
+const { getAll, create } = CustomerController;
+
+export async function GET(request: NextRequest) {
+  const { searchParams } = request.nextUrl;
+
+  const result = await getAll(searchParams);
+
+  return NextResponse.json(result.body, { status: result.status });
+}
+
+
+export async function POST(resquest: NextRequest){
+    const body = await request.json()
+
+    const result = await create(body)
+
+    return NextResponse.json(
+        result.body,
+        {status: result.status}
+    )
+}
